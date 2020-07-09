60097edbc1ae14caa11211030c1b1a54c15be51a
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-07-09 13:46:35 +0300

    created UserRepository interface and  added getters and setters of userEntity and userDto classes

---
+++ src/main/java/com/bahadir/mobile/app/ws/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bahadir.mobile.app.ws;
+
+import com.bahadir.mobile.app.ws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
