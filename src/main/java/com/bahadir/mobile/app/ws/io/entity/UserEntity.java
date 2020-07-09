2139003ac3c04a76cf22d66b7b6f183a64ae6d1e
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-07-09 16:52:33 +0300

    preventing duplicates checking email address but not responds with proper response message

--- src/main/java/com/bahadir/mobile/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/bahadir/mobile/app/ws/io/entity/UserEntity.java
@@ -13,7 +13,7 @@ public class UserEntity implements Serializable {
     @GeneratedValue
     private long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String userId;
 
     @Column(nullable=false, length = 50)
@@ -22,7 +22,7 @@ public class UserEntity implements Serializable {
     @Column(nullable=false, length = 50)
     private String lastName;
 
-    @Column(nullable=false, length = 120, unique = true)
+    @Column(nullable=false, length = 120)
     private String email;
 
     @Column(nullable=false)
