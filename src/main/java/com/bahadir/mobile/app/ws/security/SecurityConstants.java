8524611a2d1edb2ee02958a082486b6f924944ee
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-07-13 16:19:08 +0300

    securyityConstants and bug fix

--- src/main/java/com/bahadir/mobile/app/ws/security/SecurityConstants.java
+++ src/main/java/com/bahadir/mobile/app/ws/security/SecurityConstants.java
@@ -1,4 +1,9 @@
 package com.bahadir.mobile.app.ws.security;
 
 public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 86400000;
+    public static final String TOKEN_PREFIX= "Bearer ";
+    public static final String HEADER_STRING= "Authorization";
+    public static final String SIGN_UP_URL= "/users";
+    public static final String TOKEN_SECRET = "jf9kakjakdks21fl";
 }
